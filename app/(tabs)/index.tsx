2e42ac167bd954257226a575d52218dbce5cedf0
Johann Gonzalez <dt.ait.programador2@cgesucre.gob.ve>
2025-03-18 15:38:18 -0400

    18/03/2025

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,11 +9,10 @@ interface ServiceCardProps {
 
 const ServiceCard: React.FC<ServiceCardProps> = ({ title, description, imageSource }) => (
   <ImageBackground source={imageSource} style={styles.card}>
-  <View style={styles.textContainer}>
-    <Text style={styles.cardTitle}>{title}</Text>
-    <Text style={styles.cardDescription}>{description}</Text>
-  </View>
-</ImageBackground>
+    <View style={styles.textContainer}>
+      <Text style={styles.cardTitle}>{title}</Text>
+    </View>
+  </ImageBackground>
 );
 
 const HomeScreen: React.FC = () => {
@@ -40,44 +39,43 @@ const HomeScreen: React.FC = () => {
         <ServiceCard
           title="Commercial Construction"
           description=" "
-          imageSource={require('@/assets/images/commercial.jpg')} 
+          imageSource={require('@/assets/images/commercial.jpg')}
         />
         <ServiceCard
           title="Residential Construction"
           description=" "
-          imageSource={require('@/assets/images/commercial.jpg')} 
+          imageSource={require('@/assets/images/residential.jpg')}
         />
         <ServiceCard
           title="Pre-Construction"
           description=" "
-          imageSource={require('@/assets/images/commercial.jpg')} 
+          imageSource={require('@/assets/images/preconstruction.jpg')}
         />
         <ServiceCard
           title="Special Projects"
           description=" "
-          imageSource={require('@/assets/images/commercial.jpg')}
+          imageSource={require('@/assets/images/specialprojects.jpg')}
         />
         <ServiceCard
           title="Site Management"
           description=" "
-          imageSource={require('@/assets/images/commercial.jpg')} // Agrega la imagen
+          imageSource={require('@/assets/images/sitemanagement.jpg')}
         />
         <ServiceCard
           title="Infrastructure Construction"
           description=" "
-          imageSource={require('@/assets/images/commercial.jpg')} // Agrega la imagen
+          imageSource={require('@/assets/images/infrastructureConstruction.jpg')}
         />
         <ServiceCard
           title="Civil Engineering"
           description=" "
-          imageSource={require('@/assets/images/commercial.jpg')} // Agrega la imagen
+          imageSource={require('@/assets/images/civilengineering.jpg')}
         />
         <ServiceCard
           title="Landscape Construction"
           description=" "
-          imageSource={require('@/assets/images/commercial.jpg')} // Agrega la imagen
+          imageSource={require('@/assets/images/landscape.jpg')}
         />
-        
       </ScrollView>
     </View>
   );
@@ -85,10 +83,10 @@ const HomeScreen: React.FC = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
   },
   backgroundImage: {
-    height: 170, 
+    height: 170,
     justifyContent: 'flex-start',
   },
   searchContainer: {
@@ -101,7 +99,7 @@ const styles = StyleSheet.create({
     marginTop: 60,
   },
   textContainer: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
     padding: 16,
     width: '100%',
     alignItems: 'center',
@@ -127,17 +125,21 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   card: {
-    height: 150, 
+    height: 120,
+    width: '100%',
     borderRadius: 8,
-    overflow: 'hidden', 
+    overflow: 'hidden',
     marginBottom: 16,
-    justifyContent: 'center', 
-    alignItems: 'center', 
+    justifyContent: 'center',
+    alignItems: 'center',
+    aspectRatio: 16/9,
+    resizeMode: 'cover'
   },
   cardTitle: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 8,
+    color: 'white'
   },
   cardDescription: {
     fontSize: 16,
