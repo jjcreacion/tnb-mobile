97ad8d9768555702d8aa2e4f0860d6c2591f9e88
Johann Gonzalez <dt.ait.programador2@cgesucre.gob.ve>
2025-05-12 12:46:13 -0400

    Json Update

--- app/(screens)/RequestModal.tsx
+++ app/(screens)/RequestModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   Modal,
   View,
@@ -8,33 +8,23 @@ import {
   ScrollView,
   StyleSheet,
   Button,
-  Platform,
 } from 'react-native';
 import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
+import { Picker } from '@react-native-picker/picker';
 import * as ImagePicker from 'expo-image-picker';
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
-
-interface Service {
-  codigo: number;
-  title: string;
-  icon: string;
-  color: string;
-}
+import MapView, { Marker } from 'react-native-maps'; // Importa MapView y Marker
 
 interface ModalProps {
   isVisible: boolean;
   onClose: () => void;
-  selectedService: Service | null;
 }
 
-const Request: React.FC<ModalProps> = ({ isVisible, onClose, selectedService }) => {
+const Request: React.FC<ModalProps> = ({ isVisible, onClose }) => {
+  const [selectedService, setSelectedService] = useState('');
   const [images, setImages] = useState([]);
-  const [latitude, setLatitude] = useState<number | null>(null);
-  const [longitude, setLongitude] = useState<number | null>(null);
-  const [region, setRegion] = useState({
+  const [mapRegion, setMapRegion] = useState({ // Estado para las coordenadas del mapa
     latitude: 37.78825,
     longitude: -122.4324,
     latitudeDelta: 0.0922,
@@ -53,29 +43,8 @@ const Request: React.FC<ModalProps> = ({ isVisible, onClose, selectedService }) 
     }
   };
 
-  const getLocation = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      console.log('Permission to access location was denied');
-      return;
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    setLatitude(location.coords.latitude);
-    setLongitude(location.coords.longitude);
-    setRegion({
-      latitude: location.coords.latitude,
-      longitude: location.coords.longitude,
-      latitudeDelta: 0.02,
-      longitudeDelta: 0.02,
-    });
-  };
-
-  useEffect(() => {
-    getLocation();
-  }, []);
-
   const validationSchema = Yup.object().shape({
+    requirement: Yup.string().required('Requirement is required'),
     description: Yup.string().required('Description is required'),
     address: Yup.string().required('Address is required'),
   });
@@ -93,48 +62,50 @@ const Request: React.FC<ModalProps> = ({ isVisible, onClose, selectedService }) 
             <FontAwesome name="close" size={24} color="black" />
           </TouchableOpacity>
 
-          {selectedService && (
-            <View style={styles.iconContainer}>
-              <MaterialIcons name={selectedService.icon} size={80} color={selectedService.color} />
-              <Text style={styles.titleText}>{selectedService.title}</Text>
-            </View>
-          )}
+          <View style={styles.iconContainer}>
+            <MaterialIcons name="business" size={70} color="#f54021" />
+            <Text style={styles.titleText}>Commercial Construction</Text>
+          </View>
 
           <Formik
             initialValues={{
-              service: selectedService ? selectedService.title : '',
+              service: '',
               requirement: '',
               description: '',
               address: '',
             }}
             validationSchema={validationSchema}
             onSubmit={(values) => {
-              const serviceRequestData = {
-                fkUser: 6, // Usuario por defecto
-                serviceType: selectedService?.codigo || 0,
-                serviceDescription: values.description,
-                address: values.address,
-                latitude: latitude !== null ? latitude : 0,
-                longitude: longitude !== null ? longitude : 0,
-              };
-              console.log(serviceRequestData, images);
+              console.log(values, images);
               onClose();
-              // Aquí iría la lógica para enviar los datos a tu API
-              // fetch('http://localhost:8081/service_request', {
-              //   method: 'POST',
-              //   headers: {
-              //     'Content-Type': 'application/json',
-              //   },
-              //   body: JSON.stringify(serviceRequestData),
-              // })
-              // .then(response => response.json())
-              // .then(data => console.log('Success:', data))
-              // .catch((error) => console.error('Error:', error));
             }}
-            enableReinitialize
           >
             {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
               <View>
+                <Picker
+                  selectedValue={selectedService}
+                  onValueChange={(itemValue) => {
+                    setSelectedService(itemValue);
+                    handleChange('service')(itemValue);
+                  }}
+                  style={styles.input}
+                >
+                  <Picker.Item label="Select a Service" value="" />
+                  <Picker.Item label="Plumbing" value="plumbing" />
+                  <Picker.Item label="Electrical" value="electrical" />
+                  <Picker.Item label="Carpentry" value="carpentry" />
+                </Picker>
+
+                <TextInput
+                  style={styles.input}
+                  placeholder="Requirement"
+                  onChangeText={handleChange('requirement')}
+                  onBlur={handleBlur('requirement')}
+                  value={values.requirement}
+                />
+                {touched.requirement && errors.requirement && (
+                  <Text style={styles.errorText}>{errors.requirement}</Text>
+                )}
 
                 <TextInput
                   style={styles.descriptionInput}
@@ -158,29 +129,12 @@ const Request: React.FC<ModalProps> = ({ isVisible, onClose, selectedService }) 
                 {touched.address && errors.address && (
                   <Text style={styles.errorText}>{errors.address}</Text>
                 )}
-
-                <View style={styles.mapContainer}>
-                  <MapView
-                    style={styles.map}
-                    region={region}
-                    onRegionChangeComplete={newRegion => setRegion(newRegion)}
-                    onPress={(event) => {
-                      setLatitude(event.nativeEvent.coordinate.latitude);
-                      setLongitude(event.nativeEvent.coordinate.longitude);
-                    }}
-                  >
-                    {latitude && longitude && (
-                      <Marker
-                        coordinate={{ latitude, longitude }}
-                        title="Ubicación Seleccionada"
-                      />
-                    )}
-                  </MapView>
-                  <TouchableOpacity style={styles.gpsButton} onPress={getLocation}>
-                    <MaterialIcons name="my-location" size={24} color="white" />
-                  </TouchableOpacity>
-                </View>
-
+                <MapView
+                  style={styles.map}
+                  region={mapRegion}
+                >
+                    <Marker coordinate = {mapRegion}/>
+                </MapView>
                 <View style={styles.buttonContainer}>
                   <View style={styles.button} >
                     <Button title="Upload Images" color="#f54021" onPress={handleImagePicker} />
@@ -189,7 +143,6 @@ const Request: React.FC<ModalProps> = ({ isVisible, onClose, selectedService }) 
                     <Button title="Save" onPress={handleSubmit} />
                   </View>
                 </View>
-
               </View>
             )}
           </Formik>
@@ -199,6 +152,7 @@ const Request: React.FC<ModalProps> = ({ isVisible, onClose, selectedService }) 
   );
 };
 
+
 const styles = StyleSheet.create({
   modalContainer: {
     flex: 1,
@@ -206,22 +160,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
+  map: {
+    width: '100%', 
+    height: 150, 
+    marginTop: 5, 
+  },
   modalContent: {
     backgroundColor: 'white',
     padding: 20,
     borderRadius: 10,
-    width: '90%',
-    maxHeight: '90%',
+    width: '95%',
+    maxHeight: '95%',
   },
   closeButton: {
     alignSelf: 'flex-end',
   },
   iconContainer: {
     alignItems: 'center',
-    marginBottom: 20,
+    marginBottom: 10,
   },
   titleText: {
-    marginTop: 10,
+    marginTop: 0,
     fontSize: 18,
     fontWeight: 'bold',
   },
@@ -232,47 +191,27 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderRadius: 5,
   },
-  descriptionInput: {
+  descriptionInput: { 
     borderWidth: 1,
     borderColor: '#ccc',
     padding: 10,
-    marginBottom: 20,
+    marginBottom: 20, 
     borderRadius: 5,
-    height: 100,
-    textAlignVertical: 'top',
+    height: 100, 
+    textAlignVertical: 'top', 
   },
   errorText: {
     color: 'red',
     marginBottom: 10,
   },
-  buttonContainer: {
-    marginTop: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+  buttonContainer: { 
+    marginTop: 20, 
+    justifyContent: 'space-around', 
   },
   button: {
-    flex: 1,
+    flex: 1, 
     marginTop: 10,
-    marginHorizontal: 5,
-  },
-  mapContainer: {
-    height: 200,
-    width: '100%',
-    borderRadius: 5,
-    overflow: 'hidden',
-    marginBottom: 10,
-    position: 'relative',
-  },
-  map: {
-    flex: 1,
-  },
-  gpsButton: {
-    position: 'absolute',
-    bottom: 10,
-    right: 10,
-    backgroundColor: '#007bff',
-    borderRadius: 20,
-    padding: 10,
+    marginHorizontal: 5, 
   },
 });
 
