de8a618ea1dd9fac7c2008382c066e9bdc266d24
rudy-rojas <rudy.rs1@gmail.com>
2025-09-02 14:36:13 -0400

    Fixed warnings of Modal Missing/splash

--- app/(screens)/_layout.tsx
+++ app/(screens)/_layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout() {
   return (
     <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="splash" options={{ headerShown: false }} /> 
+        {/* <Stack.Screen name="splash" options={{ headerShown: false }} /> */}
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="ShareAndEarn" options={{ headerShown: false }} />
         <Stack.Screen name="TermsAndPolicies" options={{ headerShown: false }} />
