76f3d33f7ea3d9e9eadb49d12cf95ceee29276c5
rudy-rojas <rudy.rs1@gmail.com>
2025-10-09 11:53:34 -0400

    Add blur on login

--- app/(screens)/login.tsx
+++ app/(screens)/login.tsx
@@ -1,14 +1,14 @@
+import { Button, Input, Screen } from '@/components/common';
+import { Theme } from '@/constants/Theme';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Constants from 'expo-constants';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
 import { Formik } from 'formik';
 import React, { useState } from 'react';
-import { Image, ImageBackground, StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { Image, ImageBackground, KeyboardAvoidingView, Platform, StyleSheet, Text, View } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import * as Yup from 'yup';
-import { Button, Input, Screen } from '@/components/common';
-import { Theme } from '@/constants/Theme';
 import ResetModal from './resetPassword';
 import SignUpModal from './singup';
 
@@ -63,9 +63,10 @@ export default function LoginScreen() {
     <ImageBackground
       source={require('@/assets/images/ingenieros.jpeg')}
       style={styles.backgroundImage}
+      blurRadius={10}
     >
       <LinearGradient
-        colors={['rgba(230, 57, 70, 0.3)', 'rgba(230, 57, 70, 0.6)', 'rgba(230, 57, 70, 0.8)']}
+       colors={['rgba(230, 57, 70, 0.2)', 'rgba(230, 57, 70, 0.3)', 'rgba(230, 57, 70, 0.6)']}
         style={styles.overlay}
       />
 
