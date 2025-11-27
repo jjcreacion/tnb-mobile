758c5fd70857f66f239c2596024188388d55abf6
Johann <jjcreacion@gmail.com>
2025-11-27 10:19:02 -0400

    [Add][Notificaciones] Activar - Desactivar notificaciones push

--- store/slices/deviceSlice.ts
+++ store/slices/deviceSlice.ts
@@ -130,11 +130,9 @@ export const updateDevicePreferences = createAsyncThunk<
     async (notificationsEnabled: boolean, { rejectWithValue }) => {
         
         const token = await AsyncStorage.getItem('deviceToken');
-        console.log("token ="+token);
         if (token === null) {
           return rejectWithValue('Token de dispositivo no encontrado. No se puede actualizar la preferencia.');
         }
-      console.log("notificationsEnabled ="+notificationsEnabled);
        
         try {
             const payload: UpdateDevicePreferencesDto = {
@@ -142,7 +140,6 @@ export const updateDevicePreferences = createAsyncThunk<
                 notificationsEnabled: notificationsEnabled,
             };
 
-            console.log("Payload ="+payload);
             const responseData = await deviceService.updatePreferences(payload);
             
             if (!responseData) {
